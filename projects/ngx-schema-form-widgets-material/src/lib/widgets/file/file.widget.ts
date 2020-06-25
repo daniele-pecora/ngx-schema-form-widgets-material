d6fa929d51b3f1e2cbc22a6151f9d8832d3c10bf
Daniele Pecora <daniele.pecora@gmail.com>
2020-06-25 15:16:52 +0200

    Update file.widget.ts
    fix computing file size string

--- projects/ngx-schema-form-widgets-material/src/lib/widgets/file/file.widget.ts
+++ projects/ngx-schema-form-widgets-material/src/lib/widgets/file/file.widget.ts
@@ -279,9 +279,12 @@ export class ByteSizeFormatPipe implements PipeTransform {
     return this.bytesToSize(value)
   }
 
-  bytesToSize(val: any) {
-    const f = (Math.floor(Math.log2(val) / 10))
-    const v = (val > 1000 ? (val / (f * 1000)) : val)
-    return v + ' ' + ['Bytes', 'Kb', 'Mb', 'Gb', 'Tb'][f]
+  bytesToSize(bytes: any) {
+    if (bytes === 0) return '0 Bytes';
+    const k = 1024;
+    const dm = 2;
+    const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+    const i = Math.floor(Math.log(bytes) / Math.log(k));
+    return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
   }
 }
